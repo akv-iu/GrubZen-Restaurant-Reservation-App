2e7e45f340f620cc7ee1eff3290f954fe71477c2
Akshay <akshayviswanathgrad@gmail.com>
2024-04-18 13:33:23 -0400

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-
+import {BrowserRouter , Routes, Route, Link} from 'react-router-dom'
+import Homescreen from './screens/Homescreen';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/home" exact component={Homescreen} />
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
