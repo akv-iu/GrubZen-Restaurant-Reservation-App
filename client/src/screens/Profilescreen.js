00a9941c88636e8ad6a05a40f80ba6b4b06bcbc5
Akshay <akshayviswanathgrad@gmail.com>
2024-04-22 14:39:52 -0400

    upates tyler
    updates to front end booking dollar and label change

--- client/src/screens/Profilescreen.js
+++ client/src/screens/Profilescreen.js
@@ -66,7 +66,7 @@ export const MyOrders = () => {
       setloading(true);
       const result = await axios.post('/api/bookings/cancelbooking' , {bookingid:bookingid , userid:user._id , roomid:roomid});
       setloading(false);
-      Swal.fire('Congrats' , 'Your Room has cancelled succeessfully' , 'success').then(result=>{
+      Swal.fire('Congrats' , 'Your Restaurant has cancelled succeessfully' , 'success').then(result=>{
         window.location.href='/profile'
     })
     } catch (error) {
