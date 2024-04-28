6adf257ab31b9e54726a34664f749449b7616ca7
Akshay <akshayviswanathgrad@gmail.com>
2024-04-28 18:09:12 -0400

    .

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.use('/api/bookings' , bookingsRoute)
 
 //     })
 // }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Node JS Server Started`))
